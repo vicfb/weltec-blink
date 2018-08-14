73b2dea763cd51d3f88d13df60605225c40cda75
vicfb <victorbernardes8@gmail.com>
2018-08-15 11:33:28 +1200

    program with button counter

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #define ledPin 21
-#define button 12
+#define button 14
 
 
 // constants won't change. Used here to set a pin number:
@@ -8,7 +8,9 @@
 
 // Variables will change:
 int ledState = LOW;             // ledState used to set the LED
-int buttonState = LOW;
+int buttonState; 
+int previousButtonState;
+int buttonCount;
 
 
 // Generally, you should use "unsigned long" for variables that hold time
@@ -21,18 +23,24 @@ const long interval = 1000;           // interval at which to blink (millisecond
 void setup() {
   // set the digital pin as output:
   pinMode(ledPin, OUTPUT);
-  pinMode(button, INPUT);
+  pinMode(button, INPUT_PULLUP);
+  Serial.begin(115200);
 }
 
 void loop() {
   // here is where you'd put code that needs to be running all the time.
-
   // check to see if it's time to blink the LED; that is, if the difference
   // between the current time and last time you blinked the LED is bigger than
   // the interval at which you want to blink the LED.
   unsigned long currentMillis = millis();
 
+  previousButtonState = buttonState;
   buttonState = digitalRead(button);
+  if (previousButtonState == HIGH && buttonState == LOW){
+    buttonCount++;
+    Serial.println(buttonCount);
+  }
+  
   
   if(buttonState==LOW){
       if (currentMillis - previousMillis >= interval) {
